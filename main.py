aed53de9b2114f809806e9c4f9733446ca72dc81
Alexey Veretenov <veretenoff.aa@gmail.com>
2025-01-17 17:20:40 +0300

    add main.py, publisher.py, rabbitmq.py, requirements.txt, docker-compose.yaml change .gitignore

---
+++ main.py
@@ -0,0 +1,24 @@
+from rabbitmq import RabbitMQ
+import sys
+
+
+def callback(ch, method, properties, body):
+    print(f"Received message: {body}")
+    # return
+
+
+def main():
+    rabbitmq = RabbitMQ()
+    try:
+        print("Connection to RabbitMQ established successfully.")
+        rabbitmq.consume(queue_name='test_queue', callback=callback)
+    except Exception as e:
+        print(f"Failed to establish connection to RabbitMQ: {e}")
+        sys.exit(1)
+    finally:
+        rabbitmq.close()
+    print("Work is finished.")
+
+
+if __name__ == "__main__":
+    main()
